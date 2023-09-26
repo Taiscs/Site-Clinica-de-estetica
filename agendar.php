ac193f1d7593328b18793cf9e456a9901e3188ee
Taís campos dos Santos <88733561+Taiscs@users.noreply.github.com>
2023-09-26 20:54:07 -0300

    Update agendar.php

--- agendar.php
+++ agendar.php
@@ -1,47 +1,56 @@
 <?php
-// Configurações do banco de dados
-$servername = "localhost"; // servidor do banco de dados (padrão: localhost)
-$username = "root"; // usuário do banco de dados do PHPMyAdmin (padrão: root)
-$password = ""; // senha do banco de dados do PHPMyAdmin (padrão: vazio)
-$dbname = "Tassia"; // nome do banco de dados
 
-// Criando a conexão
-$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificando a conexão
-if ($conn->connect_error) {
-    die("Falha na conexão: " . $conn->connect_error);
-}
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-// Criação da tabela "agendamento" se ainda não existir
-$sql = "CREATE TABLE IF NOT EXISTS agendamento (
-    id INT(11) AUTO_INCREMENT PRIMARY KEY,
-    Nome VARCHAR(100) NOT NULL,
-    Telefone VARCHAR(20) NOT NULL,
-    Data DATE NOT NULL,
-    Procedimento VARCHAR(50) NOT NULL
-)";
-if ($conn->query($sql) === FALSE) {
-    echo "Erro ao criar tabela: " . $conn->error;
-}
+// o arquivo de autoload da classe PHPMailer
+require __DIR__ . '/vendor/phpmailer/phpmailer/src/Exception.php';
+require __DIR__ . '/vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require __DIR__ . '/vendor/phpmailer/phpmailer/src/SMTP.php';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    
+    // Obtendo os dados do formulário
+    $nome = $_POST['nome'];
+    $telefone = $_POST['telefone'];
+    $data = $_POST['data'];
+    $hora = $_POST['hora'];
+    $procedimento = $_POST['procedimentos'];
 
-// Obtendo os dados do formulário
-$nome = $_POST['nome'];
-$telefone = $_POST['telefone'];
-$data = $_POST['data'];
-$procedimento = $_POST['procedimentos'];
 
-// Inserindo os dados na tabela "agendamento"
-$sql = "INSERT INTO agendamento (Nome, Telefone, Data, Procedimento)
-        VALUES ('$nome', '$telefone', '$data', '$procedimento')";
+        // Envio de e-mail com os dados usando PHPMailer
+        $mail = new PHPMailer(true);
 
-if ($conn->query($sql) === TRUE) {
-    echo "Agendamento salvo com sucesso!";
-} else {
-    echo "Erro ao salvar o agendamento: " . $conn->error;
-}
 
-// Fechando a conexão com o banco de dados
-$conn->close();
+        try {
+            $mail->isSMTP();
+            $mail->Host = ''; // Endereço do servidor SMTP
+            $mail->SMTPAuth = true;
+            $mail->Username = ''; // Seu endereço de e-mail
+            $mail->Password = ''; // Sua senha de e-mail
+            $mail->SMTPSecure = PHPMailer:: ENCRYPTION_STARTTLS; // tls ou ssl
+            $mail->Port = 587; // Porta do servidor SMTP
+
+            $mail->setFrom('', 'Agendamento site');
+            $mail->addAddress(''); // Endereço de e-mail de destino
+            $mail->Subject = 'Novo agendamento';
+            $mail->Body = "Novo agendamento realizado:\n\n" .
+                          "Nome: " . $nome . "\n" .
+                          "Telefone: " . $telefone . "\n" .
+                          "Data: " . $data . "\n" .
+                          "Hora: " . $hora . "\n" .
+                          "Procedimento: " . $procedimento;
+
+            if ($mail->send()) {
+                echo "Agendamento Realizado, Te aguardo na Data e Hora Marcada! S2";
+            } else {
+                echo "Erro ao enviar o e-mail: " . $mail->ErrorInfo;
+            }
+        } catch (Exception $e) {
+            echo "Erro ao enviar o e-mail: " . $e->getMessage();
+        }
+      }
+
+
+
 ?>
-
